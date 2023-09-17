91241014e50a47b53b2fedbed5ebc1562a1e71ad
kocub <kocubaostap1@gmail.com>
2023-09-17 19:34:45 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+public class Toy
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public string AgeRange { get; set; }
+    public string SpecialAttribute { get; set; }
+
+    public Toy(string name, int price, string ageRange, string specialAttribute)
+    {
+        Name = name;
+        Price = price;
+        AgeRange = ageRange;
+        SpecialAttribute = specialAttribute;
+    }
+}
+
+public class ToyInfo
+{
+    public static void Main(string[] args)
+    {
+        List<Toy> toys = ReadFile("toys.txt");
+
+        foreach (var toy in toys.Where(t => t.Name == "blocks").OrderBy(t => t.Price))
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine($"{toy.Name} - {toy.Price / 100} грн і {toy.Price % 100:D2} коп. кількість: {toy.SpecialAttribute}");
+        }
+    }
+
+    public static List<Toy> ReadFile(string fileName)
+    {
+        var toys = new List<Toy>();
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(", ");
+                    toys.Add(new Toy(parts[0], int.Parse(parts[1]), parts[2], parts[3]));
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return toys;
+    }
+}
